e53fd93e642408ffe54db56dcb805453506ad343
Freetzen <federuizgei@gmail.com>
2024-05-16 13:30:57 -0300

    404

--- Frontend/src/components/NotFound/NotFound.jsx
+++ Frontend/src/components/NotFound/NotFound.jsx
@@ -1,10 +1,12 @@
+import { Link } from 'react-router-dom'
 import style from './NotFound.module.css'
 
 const NotFound = () => {
   return (
-    <div className={style.notFoundContainer}>
-        <div>
-            404 NOT FOUND
+    <div className={style.prueba} >
+        <div className={style.loginContainer}>
+            <h2>404 NOT FOUND...</h2>
+            <Link to={'/'}><button>Home</button></Link>
         </div>
     </div>
   )
